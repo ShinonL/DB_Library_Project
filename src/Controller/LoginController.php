5d3e4addb8af774d6f8bb4e028ed9a94336033d0
Laura <laura@x-land.ro>
2021-01-10 20:58:41 +0200

    Before trying to permanently login

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -2,19 +2,25 @@
 namespace App\Controller;
 use Doctrine\ORM\Query\Expr\Join;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\User;
 
 class LoginController extends AbstractController {
+    private $session;
+
+    public function __construct(SessionInterface $session) {
+        $this->session = $session;
+    }
+
     /**
      * @Route("/login", name="app_login")
      */
     public function login() : Response {
         if(!isset($_GET['action']))
             $_GET['action'] = '';
-        if(isset($_SESSION['user']))
-            $_GET['action'] = 'validate';
         $error = null;
 
         switch($_GET['action']) {
@@ -23,13 +29,15 @@ class LoginController extends AbstractController {
                     'error' => $error
                 ]);
             case 'validate':
-                $_SESSION['user'] = $_POST['user'];
+                $_SESSION['username'] = $_POST['username'];
 
-                $user = $this->getDoctrine()->getRepository('App:User')->findBy(['username' => $_SESSION['user']]);
-                if($user != null && $user[0]->getPassword() == $_POST['password'])
+                $user = $this->getDoctrine()->getRepository('App:User')->findBy(['username' => $_SESSION['username']]);
+                if($user != null && $user[0]->getPassword() == $_POST['password']) {
+                    $this->session->set('username', 'password');
                     return $this->render('myaccount/myaccount.html.twig', [
                         'user' => $user[0]
                     ]);
+                }
                 else {
                     $error = 'Invalid username or password';
                     return $this->render('myaccount/login.html.twig', [
@@ -41,7 +49,6 @@ class LoginController extends AbstractController {
                     'error' => $error
                 ]);
             }
-
         }
     }
 
